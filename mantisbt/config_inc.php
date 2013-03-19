8e961b1be8601877d3269f5ebde037910140b131
Tom Flick <cubeflick@gmail.com>
2013-03-20 00:16:44 +0530

    Latest mantis

---
+++ mantisbt/config_inc.php
@@ -0,0 +1,26 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bugtracker';
+	$g_db_username = 'root';
+	$g_db_password = '123';
+	
+/*
+	#####################
+	# Wiki Integration
+	#####################
+	
+	# Wiki Integration Enabled?
+	$g_wiki_enable = ON;
+	
+	# Wiki Engine
+	$g_wiki_engine = 'mediawiki';
+	
+	# Wiki namespace to be used as root for all pages relating to this mantis installation.
+	$g_wiki_root_namespace = 'mantis';
+	
+	# URL under which the wiki engine is hosted. Must be on the same server.
+	#$g_wiki_engine_url = $t_protocol . '://' . $t_host . '/%wiki_engine%/';
+	$g_wiki_engine_url = $t_protocol . '://test.test/mantis/%wiki_engine%/';
+*/
+?>
